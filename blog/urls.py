4bec2652de10e7be8ce7ec14cef7445d71ebf126
Kacper Nędzyński <kacper.nedzynski10@gmail.com>
2026-01-31 20:29:05 +0100

    Add templates and styles for post list and post detail pages

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.starting_page, name='starting-page'),
     path('posts/', views.post_list, name='post-list'),
-    path('post/<int:post_id>/', views.post_detail, name='post-detail'),
+    path('post/<slug:slug>/', views.post_detail, name='post-detail-page'),
 ]
